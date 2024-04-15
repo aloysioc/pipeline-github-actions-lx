41149b14c0de8e4d2e884f3e8be69ca1effb229a
aloysioc <aloysioc@terra.com.br>
2024-04-15 18:36:42 -0300

    Provionando instância Linux

--- main.tf
+++ main.tf
@@ -1,17 +1,12 @@
 terraform {
 
-  required_version = ">= 1.0.0"
+  required_version = "~> 1.0"
 
   required_providers {
 
-    # azurerm = {
-    #   source  = "hashicorp/azurerm"
-    #   version = "3.3.0"
-    # }
-
     aws = {
       source  = "hashicorp/aws"
-      version = "4.11.0"
+      version = "~> 5.0"
     }
   }
 
